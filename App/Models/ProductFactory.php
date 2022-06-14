3e71788d547d1a186734efa784ae866696b33d82
Mohamed Fathi <sharksmardo@gmail.com>
2022-06-14 14:15:32 +0200

    removed conditional statments in ProductFactory and used dynamic product creation based on their class name

--- App/Models/ProductFactory.php
+++ App/Models/ProductFactory.php
@@ -13,13 +13,8 @@ class ProductFactory extends Factory
 
     public function createProduct($type)
     { //returns product
-        if (strcmp($type, DVD::getClassName()) == 0) {
-            return new DVD($this->db);
-        } else if (strcmp($type, Furniture::getClassName()) == 0) {
-            return new Furniture($this->db);
-        } else if (strcmp($type, Book::getClassName()) == 0) {
-            return new Book($this->db);
-        }
+        $calssName = 'App\\Models\\' . ($type);
+        return new $calssName($this->db);
     }
 
 }
